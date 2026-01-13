83b24b7fb4a82047828655cc28b6ca475537518e
Sreerag20109451 <20109451@mail.wit.ie>
2026-01-13 22:18:55 +0000

    Remove inittialisation from region

--- earthengine/regions.py
+++ earthengine/regions.py
@@ -9,29 +9,31 @@ from earthengine.auth import EarthEngineAuth
 
 class EarthEngineRegion:
     def __init__(self):
-        # Load environment variables
-        self.envpath = pathlib.Path(__file__).parent.parent / "config" / ".env"
-        load_dotenv(dotenv_path=self.envpath)
-
-        # Read service account and JSON key from env
-        self.service_account = os.getenv("SERVICE_ACCOUNT")
-        key_json_str = os.environ.get("KEY_JSON")
-        if not key_json_str:
-            raise ValueError("KEY_JSON environment variable not set!")
-        key_json_dict = json.loads(key_json_str)
-
-        # Create a temporary file for EE SDK
-        with tempfile.NamedTemporaryFile(mode="w+", suffix=".json", delete=False) as tmp:
-            json.dump(key_json_dict, tmp)
-            self.key_path = tmp.name
-
-        # Initialize Earth Engine
-        self.eeAuth = EarthEngineAuth()
-        self.eeAuth.initialize_earth_engine(service_account=self.service_account, path_to_key=self.key_path)
-
-        # Load datasets
-        self.lsib = ee.FeatureCollection("USDOS/LSIB_SIMPLE/2017")
-        self.gmba = ee.FeatureCollection("projects/geo-watch-483617/assets/gmba")
+        pass
+        # # Load environment variables
+        # self.envpath = pathlib.Path(__file__).parent.parent / "config" / ".env"
+        # load_dotenv(dotenv_path=self.envpath)
+        # # Read service account and JSON key from env
+        # self.service_account = os.getenv("SERVICE_ACCOUNT")
+        # key_json_str = os.environ.get("KEY_JSON")
+        # print(key_json_str)
+        # key_json_dict = json.loads(key_json_str)
+        # if not key_json_str:
+        #     raise ValueError("KEY_JSON environment variable not set!")
+        # key_json_dict = json.loads(key_json_str)
+        #
+        # # Create a temporary file for EE SDK
+        # with tempfile.NamedTemporaryFile(mode="w+", suffix=".json", delete=False) as tmp:
+        #     json.dump(key_json_dict, tmp)
+        #     self.key_path = tmp.name
+        #
+        # # Initialize Earth Engine
+        # self.eeAuth = EarthEngineAuth()
+        # self.eeAuth.initialize_earth_engine(service_account=self.service_account, path_to_key=self.key_path)
+        #
+        # # Load datasets
+        # self.lsib = ee.FeatureCollection("USDOS/LSIB_SIMPLE/2017")
+        # self.gmba = ee.FeatureCollection("projects/geo-watch-483617/assets/gmba")
 
     def get_regional_boundaries(self, region, dataset="lsib", band=None):
         """
