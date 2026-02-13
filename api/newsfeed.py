7c2f7d959c2a16b5cf6473b578a37a696c23d6fe
Sreerag Sathian <20109451@mail.wit.ie>
2026-02-13 12:10:29 +0000

    Implemented newsfeed api -removed error

--- api/newsfeed.py
+++ api/newsfeed.py
@@ -11,8 +11,9 @@ newsfeed = NewsFeed()
 @newsfeedrouter.get("/apiv0/newsfeed/getdailynews")
 async def getDailynews():
     try:
-        news = await newsfeed.newget_daily_news_from_redis()
-        return JSONResponse(status_code=200, content={ "message" : "success", "data" : news})
+        newsdata = await newsfeed.get_daily_news_from_redis()
+        print(newsdata)
+        return JSONResponse(status_code=200, content={ "message" : "success", "data" : newsdata})
     except Exception as e:
         print(e.__traceback__)
         return JSONResponse(status_code=500, content={ "message" : "Error retrieving data", "data" : news})
